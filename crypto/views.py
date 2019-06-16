60b875a361ce4ede3c6ea2f2f0871c06c925174b
Nazokat Isamova <isamovanazokat@gmail.com>
2019-06-16 13:40:54 -0500

    Design of Main page Display with Bootstrap components

--- crypto/views.py
+++ crypto/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Added this to give instruction to urls.py-crypto file to request/render hoempage
 def home(request):
-     return render(request, 'home.html', {})
+    import requests
+    import json
+    #Api and Pyton/Django code for currency ticker NSI
+    price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,XML,ADA,USDT,MIOTA,TRX&tsyms=USD")
+    price = json.loads(price_request.content)
+
+    #Api and Pyhton/Django code for Cryto News NSI
+    api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api = json.loads(api_request.content)
+    return render(request, 'home.html', {'api': api, 'price':price}) # python dictionary example NSI.
